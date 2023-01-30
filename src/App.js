562643c44d4c701ccc8ac799cc7909c387e2fad1
Drew Neece <dneece5@aggienetwork.com>
2023-01-30 15:50:41 -0600

    add routing for each component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Resume from './pages/Resume'
 
 function App() {
   return (
     < >
-    
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/resume' element={<Resume />} />
+      </Routes>
     </>
   );
 }
