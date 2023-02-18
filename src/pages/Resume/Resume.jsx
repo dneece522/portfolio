654f54335db9759a959ac5258cb17d128d3dcbf2
Drew Neece <dneece5@aggienetwork.com>
2023-02-18 15:00:28 -0600

    style About and Resume pages

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,11 +1,13 @@
+import styles from './Resume.module.css'
+
 const Resume = () => {
   return (
-    < >
+    <div className={styles.container} style={{fontFamily: 'Montserrat'}}>
       <h1>Resume</h1>
       <p>
         Click the link to download <a href="resume.pdf" target="_blank" rel="noreferrer">my resume</a>!
       </p>
-    </>
+    </div>
   )
 }
 
