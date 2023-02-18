ab630aba29f60a0422cba311150095d387288f30
Drew Neece <dneece5@aggienetwork.com>
2023-02-18 14:22:12 -0600

    style Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
+import styles from './Home.module.css'
+
 const Home = () => {
   return (
-    < >
+    <div className={styles.container}>
       <h1>Andrew Neece</h1>
       <img src="profile.jpeg" alt="Andrew Neece" />
       <h2>Fullstack Software Engineer</h2>
-    </>
+    </div>
   )
 }
 
