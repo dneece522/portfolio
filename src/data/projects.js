fdeb736cf4dd0ec965c176a8aaa676cc2ca16e11
Drew Neece <dneece5@aggienetwork.com>
2023-02-19 18:33:40 -0600

    add exchange-o-gram to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,6 +12,13 @@ const projects = [
     image: '/gameplay.png',
     repositoryLink: 'https://github.com/dneece522/2k-create-a-team',
     deploymentLink: 'https://drewneece-create-a-team.fly.dev/'
+  },
+  {
+    title: 'Exchange-O-Gram',
+    description: "Exchange-O-Gram is a social media photo-sharing application. After signing up, a user can upload photos and display them on the feed page and their profile page. The user can also view other users' photos on the feed page and like and comment on each post. If a user wants to message another user directly, they can send and respond to messages from their profile page. Exchange-O-Gram makes it easy to keep up with your friends and family's lives through photo sharing!",
+    image: '/feed.png',
+    repositoryLink: 'https://github.com/dneece522/exchangeagram-front-end',
+    deploymentLink: 'https://exchangeagram.netlify.app/'
   }
 ]
 
