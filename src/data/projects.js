1935679a7d6ac2ca796947b625b00cc83f7072c9
Drew Neece <dneece5@aggienetwork.com>
2023-03-21 20:44:58 -0500

    add new project to portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,6 +19,13 @@ const projects = [
     image: '/feed.png',
     repositoryLink: 'https://github.com/dneece522/exchangeagram-front-end',
     deploymentLink: 'https://exchangeagram.netlify.app/'
+  },
+  {
+    title: 'setSchedule',
+    description: "All college students have been there; you sign up for classes months before the semester starts, and by the time classes roll around, it's hard to remember what days you have math or what time history starts. You may have a low-resolution screenshot on your phone that you took from the terrible UI on your school's website, and you may even set that as your screensaver to try and remember everything! The worst part is when your friends text you to hang out or grab some drinks at happy hour, but you just sat down for your STATS 101 class like you told your friends for the millionth time. This is all a thing of the past with setSchedule! This app allows users to create a profile to add their schedule for the semester, so they always remember when their classes are. Users can add each class's subject, code, days, time, and credit hours. Every user's schedule will be displayed on their profile, so you'll always be able to see when your friends are in class—made a mistake when submitting a class? No worries! Every user can edit a class after it's added. Once the semester is over, there is an option to delete your account so you can start fresh for the next semester or if you're graduating!",
+    image: '/landing-page.png',
+    repositoryLink: 'https://github.com/dneece522/set-schedule-front-end',
+    deploymentLink: 'https://set-schedule.netlify.app/'
   }
 ]
 
